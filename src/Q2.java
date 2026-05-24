1ac9e8eab49240bf799e0ac82d5b23d13106e55a
YujiSoftware <yuji.software+github@gmail.com>
2026-05-24 13:56:46 +0900

    確認用コードを追加

---
+++ src/Q2.java
@@ -0,0 +1,6 @@
+void main() {
+    IO.println("0: " + Character.isEmoji('0'));
+    IO.println("?: " + Character.isEmoji('?'));
+    IO.println("※: " + Character.isEmoji('※'));
+    IO.println("♫: " + Character.isEmoji('♫'));
+}
